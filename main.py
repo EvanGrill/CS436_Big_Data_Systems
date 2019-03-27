ecd322a9ba6285977109d0e592be1f6b81b981c4
Evan Grill <grille@unr.edu>
2019-03-27 15:15:13 -0700

    Calculates PageRank (Takes a while)

--- main.py
+++ main.py
@@ -2,21 +2,44 @@ from pyspark import SparkContext, SparkConf
 from operator import add
 import time
 
+def prepData(x):
+    split = x.split('\t')
+    return [split[0],split[9:]]
+
+def genGraph(x):
+    return [(w,x[0]) for w in x[1]]
+
+def computeContribs(urls, rank):
+    """Calculates URL contributions to the rank of other URLs."""
+    num_urls = len(urls)
+    for url in urls:
+        yield (url, rank / num_urls)
+
+
 def main():
     sc = SparkContext("local[8]", "YTData")
-    data = "./data/*"
+    data = "./data_short/*"
     input = sc.textFile(data)
     start = time.time()
-    results = input.map(lambda x: x.split('\t'))
-    results = results.map(lambda x: [x[0],x[9:]])
-    results = results.flatMap(lambda x: [[w,1] for w in x[1]])
-    results = results.reduceByKey(add)
-    results = results.sortBy(lambda x: x[1], ascending=False).take(10)
+    data = input.map(prepData)
+    links = data.flatMap(genGraph).distinct().groupByKey().cache()
+    ranks = links.map(lambda x: (x[0], 1.0))
+    for i in range(10):
+        contribs = links.join(ranks).flatMap(lambda x: computeContribs(x[1][0],x[1][1]))
+        ranks = contribs.reduceByKey(add).mapValues(lambda x: x * 0.85 + 0.15)
+    
+    for (link, rank) in ranks.sortBy(lambda x: x[1], ascending=False).take(10):
+        print("%s has rank: %s." % (link, rank))
+
+    #results = results.flatMap(lambda x: [[w,1] for w in x[1]])
+    #results = results.reduceByKey(add)
+    #results = results.sortBy(lambda x: x[1], ascending=False).take(10)
     end = time.time()
-    for entry in results:
-        print(entry[0], entry[1], sep=": ")
+    #for entry in results:
+    #    print(entry[0], entry[1], sep=": ")
     count = input.count()
     print( str(count) + " videos processed in " + str(end - start) + " seconds." )
+    sc.stop()
 
 if __name__ == "__main__":
     main()
