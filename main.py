d0958506dbaa5170843f845a01c11307554a9be5
Evan Grill <grille@unr.edu>
2019-04-23 23:36:05 -0700

    Added top categories (by views).

--- main.py
+++ main.py
@@ -16,51 +16,67 @@ def computeContribs(urls, rank):
     for url in urls:
         yield (url, rank / num_urls)
 
-def pageRank(input):
-    data = prepData(input)
+def pageRank(input, number):
+    data = input.filter(lambda x: len(x) > 9)
     data = data.map(lambda x: [x[0],x[9:]])
     links = data.flatMap(genGraph).distinct().groupByKey().cache()
     ranks = links.map(lambda x: (x[0], 1.0))
     for i in range(5):
         contribs = links.join(ranks).flatMap(lambda x: computeContribs(x[1][0],x[1][1]))
         ranks = contribs.reduceByKey(add).mapValues(lambda x: x * 0.85 + 0.15)
-    return ranks
+    ranks.sortBy(lambda x: x[1], ascending=False)
+    return ranks.take(number)
 
-def topCategories(input, number):
-    data = prepData(input)
-    data = data.map(lambda x: (x[3],1))
+def topCategoriesByVideos(input, number):
+    data = input.map(lambda x: (x[3],1))
     #data = data.groupByKey()
     data = data.reduceByKey(add)
     output = data.sortBy(lambda x: x[1], ascending=False)
     return output.take(number)
 
 def topRated(input, number):
-    data = prepData(input)
-    data = data.map(lambda x: (x[0],float(x[6])))
+    data = input.map(lambda x: (x[0],float(x[6])))
     data = data.sortBy(lambda x: x[1], ascending=False)
     return data.take(number)
 
+def topCategoriesByViews(input, number):
+    data = input.map(lambda x: (x[3], int(x[5])))
+    data = data.reduceByKey(add)
+    output = data.sortBy(lambda x: x[1], ascending=False)
+    return output.take(number)
 
 
 def main():
     sc = SparkContext("local[8]", "YTData")
-    data = "./data/*"
+    data = "./data_short/*"
     input = sc.textFile(data)
+    input = prepData(input)
     start = time.time()
-    #print(topCategories(input, 10))
-    print(topRated(input, 10))
-    #ranks = pageRank(input)
-    #for (link, rank) in ranks.sortBy(lambda x: x[1], ascending=False).take(10):
-    #    print("%s has rank: %s." % (link, rank))
-
-    #results = results.flatMap(lambda x: [[w,1] for w in x[1]])
-    #results = results.reduceByKey(add)
-    #results = results.sortBy(lambda x: x[1], ascending=False).take(10)
+    catVideos = topCategoriesByVideos(input, 5)
+    catViews = topCategoriesByViews(input, 5)
+    rates = topRated(input, 10)
+    #ranks = pageRank(input, 10)
     end = time.time()
-    #for entry in results:
-    #    print(entry[0], entry[1], sep=": ")
-    count = input.count()
-    print( str(count) + " videos processed in " + str(end - start) + " seconds." )
+    vidCount = input.count()
+
+    #print("Top 10 Videos by PageRank:")
+    #for id, rank in ranks:
+    #    print("Video:", id, "Rank:", rank)
+    #print(" ")
+    print("Top 10 Videos by Rating:")
+    for id, rating in rates:
+        print("Video:", id, "Rating:", rating)
+    print(" ")
+    print("Top 5 Categories (by Videos):")
+    for cat, count in catVideos:
+        print("Category:", cat, "Count:", count)
+    print(" ")
+    print("Top 5 Categories (by Views):")
+    for cat, count in catViews:
+        print("Category:", cat, "Count:", count)
+    print(" ")
+
+    print( str(vidCount) + " videos processed in " + str(end - start) + " seconds." )
     sc.stop()
 
 if __name__ == "__main__":
