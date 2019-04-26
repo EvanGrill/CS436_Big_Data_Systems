c55e84242a85173f997612de783c2ed2154402a7
Evan Grill <grille@unr.edu>
2019-04-25 18:02:49 -0700

    Added top uploaders.

--- main.py
+++ main.py
@@ -48,10 +48,15 @@ def topCategoriesByViews(input, number):
 
 #added for comments
 def topComments(input,number):
-    data=input.map(lambda x: (str(x[0]), str(x[3]),int(x[8])))
+    data=input.map(lambda x: (x[0], x[3], int(x[8])))
     output=data.sortBy(lambda x: x[2], ascending=False)
     return output.take(number)
 
+def topUploaders(input, number):
+    data = input.map(lambda x: (x[1], 1))
+    data = data.reduceByKey(add)
+    output = data.sortBy(lambda x: x[1], ascending=False)
+    return output.take(number)
 
 def main():
     locale.setlocale(locale.LC_ALL, 'en_US')
@@ -64,7 +69,8 @@ def main():
     catViews = topCategoriesByViews(input, 5)
     rates = topRated(input, 10)
     #added for comments
-    comments=topComments(input,10)
+    comments = topComments(input,10)
+    uploads = topUploaders(input, 10)
     #ranks = pageRank(input, 10)
     end = time.time()
     vidCount = input.count()
@@ -85,10 +91,11 @@ def main():
     for cat, count in catViews:
         print("Category: ", cat, "| Views:", locale.format_string("%d", count, grouping=True))
     print(" ")
-
-    #added for comments
     for id, cat, com in comments:
         print("Video:", id, "Category:", cat, "Comments:", com)
+    print(" ")
+    for user, count in uploads:
+        print("User:", user, "| Uploads:", count)
     print(" ")
 
     print( str(vidCount) + " videos processed in " + str(end - start) + " seconds." )
